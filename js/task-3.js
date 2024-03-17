b258824f09f64201bd4d6dff0ad27199b9bf04fd
ann-shevchenko <154280836+ann-shevchenko@users.noreply.github.com>
2024-03-17 22:29:41 +0100

    fix defects + code formatting

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,15 @@
-"use strict"
+"use strict";
 
 function checkForSpam(message) {
-    message = message.toLowerCase();
-    return message.includes("spam") || message.includes("sale");
+  const lowerCaseMsg = message.toLowerCase();
+  return lowerCaseMsg.includes("spam") || lowerCaseMsg.includes("sale");
 }
+
+console.log("Task #3");
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
